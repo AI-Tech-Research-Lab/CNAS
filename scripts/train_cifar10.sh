3f4bf54aac9408036f2ea27c965f5b76073b40c5
Matteo <matteogambella@gmail.com>
2022-12-16 16:20:41 +0100

    update train-cifar

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -7,6 +7,6 @@ python adaptive_train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --model1-config ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@3/net.config \
     --model2-config ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@93010000012/net.config \
     --initial-checkpoint1 ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@3/net.inherited \
-    --initial-checkpoint2 ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@93010000012/net.inherited \ 
+    --initial-checkpoint2 ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@93010000012/net.inherited \
     --drop 0.2 --drop-path 0.2 \
     --img-size 40
