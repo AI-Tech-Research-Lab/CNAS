1e5aaeed0acfa59760b02394095b9c049c47ea11
Matteo <matteogambella@gmail.com>
2022-12-20 16:47:00 +0100

    test

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -4,7 +4,7 @@ python train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --epochs 5 \
     --cutout --autoaugment \
     --save ../results \
-    --evaluate \ 
+    --evaluate \
     --model adapt_tinynsganet \
     --model-config ../results/20221220-154050cifar10adapt_tinynsganet40/net_flops@7.config \
     --initial-checkpoint ../results/20221220-154050cifar10adapt_tinynsganet40/net_flops@7.best \
