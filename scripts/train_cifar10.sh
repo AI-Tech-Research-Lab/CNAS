dbe91e690dea992d3600fd18bac679182cc0d740
Matteo <matteogambella@gmail.com>
2022-12-20 16:45:58 +0100

    test

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -4,7 +4,9 @@ python train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --epochs 5 \
     --cutout --autoaugment \
     --save ../results \
+    --evaluate \ 
     --model adapt_tinynsganet \
-    --model-config ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@7.config \
+    --model-config ../results/20221220-154050cifar10adapt_tinynsganet40/net_flops@7.config \
+    --initial-checkpoint ../results/20221220-154050cifar10adapt_tinynsganet40/net_flops@7.best \
     --drop 0.2 --drop-path 0.2 \
     --img-size 40
