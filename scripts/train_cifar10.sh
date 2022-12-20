51413b542ebba53d32f3bf7fa9852151b54202f7
Matteo <matteogambella@gmail.com>
2022-12-20 15:33:48 +0100

    test

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -7,7 +7,7 @@ python adaptive_train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --model adapt_tinynsganet \
     --model1-config ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@7.config \
     --model2-config ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@23.config \
-    --initial-checkpoint1 ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@7.config \
-    --initial-checkpoint2 ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@23.config \
+    --initial-checkpoint1 ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@7.best \
+    --initial-checkpoint2 ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@23.best \
     --drop 0.2 --drop-path 0.2 \
     --img-size 40
