42a7963204953cfb5b2464abf3aaba4d0d009a96
Matteo <matteogambella@gmail.com>
2022-12-20 12:45:37 +0100

    test

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -3,10 +3,11 @@ cd ../nsganetv2
 python adaptive_train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --epochs 20 \
     --cutout --autoaugment --save ../results \
+    --evaluate \
     --model adapt_tinynsganet \
-    --model1-config ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@3/net.config \
-    --model2-config ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@93010000012/net.config \
-    --initial-checkpoint1 ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@3/net.inherited \
-    --initial-checkpoint2 ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/final/net-tiny_ml@93010000012/net.inherited \
+    --model1-config ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@7.config \
+    --model2-config ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@23.config \
+    --initial-checkpoint1 ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@7.config \
+    --initial-checkpoint2 ../results/20221220-104528cifar10adapt_tinynsganet40/net_flops@23.config \
     --drop 0.2 --drop-path 0.2 \
     --img-size 40
