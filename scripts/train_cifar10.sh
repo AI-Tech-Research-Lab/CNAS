4a91bb9c2df737240e739624d586446a181c1745
Matteo <matteogambella@gmail.com>
2022-12-21 10:48:16 +0100

    test

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -1,13 +1,11 @@
 cd ../nsganetv2
 
-python adaptive_train_cifar.py --data ../data/cifar10 --dataset cifar10 \
+python train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --epochs 20 \
     --cutout --autoaugment \
     --evaluate \
-    --model adapt_tinynsganet \
-    --model1-config ../results/20221220-155540cifar10adapt_tinynsganet40/net_flops@7.config \
-    --model2-config ../results/20221220-160954cifar10adapt_tinynsganet40/net_flops@11.config \
-    --initial-checkpoint1 ../results/20221220-155540cifar10adapt_tinynsganet40/net_flops@7.best \
-    --initial-checkpoint2 ../results/20221220-160954cifar10adapt_tinynsganet40/net_flops@11.best \
+    --model tinynsganet \
+    --model-config ../results/20221220-155540cifar10adapt_tinynsganet40/net_flops@7.config \
+    --initial-checkpoint ../results/20221220-155540cifar10adapt_tinynsganet40/net_flops@7.best \
     --drop 0.2 --drop-path 0.2 \
     --img-size 40
