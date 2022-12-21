41ba9fd4a0a6c885110b0d8e1e80f66e55a9a2c7
Matteo <matteogambella@gmail.com>
2022-12-21 11:15:39 +0100

    test

--- scripts/adaptive_train_cifar10.sh
+++ scripts/adaptive_train_cifar10.sh
@@ -10,4 +10,4 @@ python adaptive_train_cifar.py --data ../data/cifar10 --dataset cifar10 \
     --initial-checkpoint1 ../results/20221220-155540cifar10adapt_tinynsganet40/net_flops@7.best \
     --initial-checkpoint2 ../results/20221220-160954cifar10adapt_tinynsganet40/net_flops@11.best \
     --drop 0.2 --drop-path 0.2 \
-    --img-size 40 --threshold 0.1
+    --img-size 40 --threshold 0.2
