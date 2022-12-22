ab36afb5cb58639420449626324189e16d0b3511
Matteo <matteogambella@gmail.com>
2022-12-22 17:01:32 +0100

    test

--- nsganetv2/test.py
+++ nsganetv2/test.py
@@ -6,10 +6,10 @@ ur = 80
 n_doe = 1
 ss_small = OFASearchSpace('mobilenetv3_small',lr,ur)
 ss_big = OFASearchSpace('mobilenetv3_big',lr,ur)
-m1_config = ss_small.initialize(n_doe)
-m2_config = ss_big.initialize(n_doe)
-print(m1_config[0])
-print(m2_config[0])
+m1_config = ss_small.initialize(n_doe)[0]
+m2_config = ss_big.initialize(n_doe)[1]
+print(m1_config)
+print(m2_config)
 
 '''
 # encode m1,m2
