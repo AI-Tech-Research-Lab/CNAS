f29415387b7b70d7a25e48c044f81841b6558e17
Matteo <matteogambella@gmail.com>
2022-12-23 11:38:21 +0100

    test

--- nsganetv2/test.py
+++ nsganetv2/test.py
@@ -5,7 +5,9 @@ lr = 40
 ur = 40
 n_doe = 1
 ss = OFASearchSpace('mobilenetv3',lr,ur)
-eval = OFAEvaluator(n_classes=10)
+eval = OFAEvaluator(n_classes=10,
+model_path='./ofa_nets/ofa_mbv3_d234_e346_k357_w1.0',
+pretrained = True)
 m1_config = ss.initialize(n_doe)[0]
 print(m1_config)
 
