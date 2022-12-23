f425cec93337376551c70232a6f85fdac802a80c
Matteo <matteogambella@gmail.com>
2022-12-23 14:53:23 +0100

    test

--- nsganetv2/test.py
+++ nsganetv2/test.py
@@ -10,6 +10,7 @@ model_path='./ofa_nets/ofa_mbv3_d234_e346_k357_w1.0',
 pretrained = True)
 m1_config = ss.initialize(n_doe)[0]
 
+'''
 m2_config = m1_config
 new_d = []
 for i in range(len(m1_config['d'])):
@@ -17,8 +18,9 @@ for i in range(len(m1_config['d'])):
     item = temp + 1
     new_d.append(item)
 m2_config['d'] = new_d
+'''
 print(m1_config)
-print(m2_config)
+#print(m2_config)
 
 
 
