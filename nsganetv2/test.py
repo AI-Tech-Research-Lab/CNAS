cd012cac950a0e43ff57720bcacd573b36e6b4ff
Matteo <matteogambella@gmail.com>
2022-12-23 11:25:11 +0100

    test

--- nsganetv2/test.py
+++ nsganetv2/test.py
@@ -1,28 +1,56 @@
 from search_space.ofa import OFASearchSpace
 from evaluator import OFAEvaluator,get_net_info
-import torch
+
+lr = 40
+ur = 40
+n_doe = 1
+ss = OFASearchSpace('mobilenetv3',lr,ur)
+eval = OFAEvaluator(n_classes=10)
+m1_config = ss.initialize(n_doe)[0]
+print(m1_config)
 
 
-#CONFIG
-n_classes = 10
-supernet = './ofa_nets/ofa_mbv3_d234_e346_k357_w1.0'
-pretrained = True
-n_epochs = 5
-trn_batch_size = 128
-vld_batch_size = 200
-img_size = 40
-data = '../data/cifar10'
-dataset = 'cifar10'
+# encode m1,m2
+m1_encode = ss.encode(m1_config)
+print(m1_encode)
 
 
-ss = OFASearchSpace(supernet = 'mobilenetv3', lr = img_size, ur = img_size)
+# decode
 
-config = ss.initialize(n_doe = 1)[0]
-evaluator = OFAEvaluator(n_classes=n_classes, model_path=supernet, pretrained = pretrained)
-subnet, _ = evaluator.sample({'ks': config['ks'], 'e': config['e'], 'd': config['d']})
-resolution = config['r']
+m1_config = ss.decode(m1_encode)
+print(m1_config)
 
-OFAEvaluator.eval(
-        subnet, data_path=data, dataset=dataset, n_epochs=n_epochs,
-        resolution=resolution, trn_batch_size=trn_batch_size, vld_batch_size=vld_batch_size)
+
+m1,_ = eval.sample(m1_config)
+
+'''
+info1 = get_net_info(m1,(40,40))
+
+'''
+
+'''
+#sample subnets from OFA through config
+
+m1,_ = eval.sample(m1_config)
+m2,_ = eval.sample(m2_config)
+'''
+
+'''
+from ofa.model_zoo import ofa_net
+
+ofa_network = ofa_net(10,'ofa_mbv3_d234_e346_k357_w1.0', pretrained=False)
+
+ofa_network.set_active_subnet(ks=7, e=6, d=3)
+m1 = ofa_network.get_active_subnet(preserve_weight=True)
+
+ofa_network.set_active_subnet(ks=7, e=6, d=4)
+m2 = ofa_network.get_active_subnet(preserve_weight=True)
+'''
+
+
+
+
+
+
+
 
