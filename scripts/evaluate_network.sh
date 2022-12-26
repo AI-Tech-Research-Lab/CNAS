9e50c3d70c727114a16e8599d5b5c0ed8cd91dc2
Matteo <matteogambella@gmail.com>
2022-12-26 11:33:19 +0100

    Revert "test"
    This reverts commit 4db9a367c823ae54c94d3042d902c84dbb1236af.

--- scripts/evaluate_network.sh
+++ scripts/evaluate_network.sh
@@ -2,5 +2,5 @@
 python evaluator.py --subnet ../benchmarks/tiny_ml/search-cifar10-mbv3-w1.0-2022-03-23/iter_0/net_0.subnet \
 --data ../data/cifar10 --dataset cifar10 \
 --n_classes 10 --supernet ./ofa_nets/ofa_mbv3_d234_e346_k357_w1.0 --pretrained \
---trn_batch_size 128 --vld_batch_size 200 --num_workers 4 --n_epochs 0 \
+--trn_batch_size 128 --vld_batch_size 200 --num_workers 4 --n_epochs 1 \
 --resolution 224 --valid_size 5000 --reset_running_statistics
