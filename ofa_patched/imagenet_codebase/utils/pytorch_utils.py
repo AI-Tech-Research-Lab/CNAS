b2c5ee9eb2651726a68f9453bc2eafbb9d7c8815
Matteo <matteogambella@gmail.com>
2023-01-04 16:02:45 +0100

    test

--- ofa_patched/imagenet_codebase/utils/pytorch_utils.py
+++ ofa_patched/imagenet_codebase/utils/pytorch_utils.py
@@ -153,7 +153,7 @@ def get_net_info(net, input_shape=(3, 224, 224), measure_latency=None, print_inf
     net_info['params'] = count_parameters(net)
     
     # flops
-    net_info['flops'] = count_net_flops(net, [1] + list(input_shape))
+    net_info['flops'] = 0#count_net_flops(net, [1] + list(input_shape))
     
     # latencies
     latency_types = [] if measure_latency is None else measure_latency.split('#')
