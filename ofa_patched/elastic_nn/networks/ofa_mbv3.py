c7db9bd36ef8676e61af68d2ec0d97c516c4309c
Matteo <matteogambella@gmail.com>
2023-01-05 16:51:56 +0100

    test

--- ofa_patched/elastic_nn/networks/ofa_mbv3.py
+++ ofa_patched/elastic_nn/networks/ofa_mbv3.py
@@ -501,7 +501,7 @@ class OFAEEMobileNetV3(EEMobileNetV3):
         d = [len(block_idx) for block_idx in self.block_group_info]
         idx_exit = d[0]+d[1]+d[2]+1
         for i in range(0,idx_exit-1,1):
-            print(blocks[idx_exit-1].mobile_inverted_conv.active_out_channel)
+            print(blocks[i].mobile_inverted_conv.active_out_channel)
         feature_dim = [blocks[idx_exit-1].mobile_inverted_conv.active_out_channel]
         super(OFAEEMobileNetV3, self).__init__(first_conv, blocks, final_expand_layer, feature_mix_layer, classifier,
         self.n_classes, final_expand_width, feature_dim, last_channel, self.dropout_rate, idx_exit)
