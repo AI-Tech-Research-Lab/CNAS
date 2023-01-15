0d55504514917e99f8b59c032d212ad36c3b22ba
Matteo <matteogambella@gmail.com>
2023-01-15 20:08:59 +0100

    test

--- ofa_patched/imagenet_codebase/run_manager/run_manager.py
+++ ofa_patched/imagenet_codebase/run_manager/run_manager.py
@@ -426,8 +426,9 @@ class RunManager:
         if net is None:
             net = self.net
         
+        print(type(net))
         n_exit = net.n_exit
-        
+
         if not isinstance(net, nn.DataParallel):
             net = nn.DataParallel(net)
 
