4d9043ed7b3f0eaabd27f0d35439f5b10d38401b
Matteo <matteogambella@gmail.com>
2023-01-15 20:53:36 +0100

    test

--- nsganetv2/evaluator.py
+++ nsganetv2/evaluator.py
@@ -475,7 +475,7 @@ def main(args):
         no_logs=(not args.verbose), reset_running_statistics=args.reset_running_statistics, 
         pmax = args.pmax, fmax = args.fmax, amax = args.amax, wp = args.wp, wf = args.wf, wa = args.wa, penalty = args.penalty)
     '''
-    '''
+    
     OFAEvaluator.adaptive_eval(
         subnet, log_dir=args.log_dir, data_path=args.data, dataset=args.dataset, n_epochs=args.n_epochs,
         resolution=resolution, trn_batch_size=args.trn_batch_size, vld_batch_size=args.vld_batch_size,
@@ -483,7 +483,7 @@ def main(args):
         no_logs=(not args.verbose), reset_running_statistics=args.reset_running_statistics, 
         pmax = args.pmax, fmax = args.fmax, amax = args.amax, wp = args.wp, wf = args.wf, wa = args.wa, penalty = args.penalty,
         )
-    '''
+    
     
     
 
