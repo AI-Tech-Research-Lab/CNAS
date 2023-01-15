2a7433f5f1827e2ec42a58ab17f7acc473c85b4d
Matteo <matteogambella@gmail.com>
2023-01-15 20:52:39 +0100

    test

--- nsganetv2/evaluator.py
+++ nsganetv2/evaluator.py
@@ -227,7 +227,7 @@ class OFAEvaluator:
         """ randomly sample a sub-network """
         if config is not None:
             #config = validate_config(config)
-            self.engine.set_active_subnet(ks=config['ks'], e=config['e'], d=config['d'])#, t=config['t'])
+            self.engine.set_active_subnet(ks=config['ks'], e=config['e'], d=config['d'], t=config['t'])
         else:
             config = self.engine.sample_active_subnet()
 
@@ -459,9 +459,9 @@ def main(args):
     elif mode == 'subnet':
         config = json.load(open(args.subnet))
         evaluator = OFAEvaluator(n_classes=args.n_classes, model_path=args.supernet, pretrained = args.pretrained)
-        subnet, _ = evaluator.sample({'ks': config['ks'], 'e': config['e'], 'd': config['d']})#, 't':config['t']})
-        threshold = config['t']
-        subnet.threshold = threshold
+        subnet, _ = evaluator.sample({'ks': config['ks'], 'e': config['e'], 'd': config['d'], 't':config['t']})
+        #threshold = config['t']
+        #subnet.threshold = threshold
         resolution = config['r']
         
 
@@ -475,6 +475,7 @@ def main(args):
         no_logs=(not args.verbose), reset_running_statistics=args.reset_running_statistics, 
         pmax = args.pmax, fmax = args.fmax, amax = args.amax, wp = args.wp, wf = args.wf, wa = args.wa, penalty = args.penalty)
     '''
+    '''
     OFAEvaluator.adaptive_eval(
         subnet, log_dir=args.log_dir, data_path=args.data, dataset=args.dataset, n_epochs=args.n_epochs,
         resolution=resolution, trn_batch_size=args.trn_batch_size, vld_batch_size=args.vld_batch_size,
@@ -482,6 +483,7 @@ def main(args):
         no_logs=(not args.verbose), reset_running_statistics=args.reset_running_statistics, 
         pmax = args.pmax, fmax = args.fmax, amax = args.amax, wp = args.wp, wf = args.wf, wa = args.wa, penalty = args.penalty,
         )
+    '''
     
     
 
