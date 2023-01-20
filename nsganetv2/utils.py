97a0514929aeb777c27d7212aad5b8b1859b97db
Matteo <matteogambella@gmail.com>
2023-01-20 20:56:51 +0100

    test

--- nsganetv2/utils.py
+++ nsganetv2/utils.py
@@ -334,14 +334,14 @@ def get_net_info(net, input_shape=(3, 224, 224), measure_latency=None, print_inf
         net = net.module
 
     # parameters
-    net_info['params'] = 0#count_parameters(net)
+    net_info['params'] = count_parameters(net)
 
     net = copy.deepcopy(net)
 
-    net_info['macs'] = 0#int(profile_macs(net, inputs))
+    net_info['macs'] = int(profile_macs(net, inputs))
    
     # activation_size
-    net_info['activations'] = 0#int(profile_activation_size(net, inputs))
+    net_info['activations'] = int(profile_activation_size(net, inputs))
 
     # latencies
     latency_types = [] if measure_latency is None else measure_latency.split('#')
