585531e408bf89c98addb20aa42fd6c2a70a86e0
Matteo <matteogambella@gmail.com>
2023-01-20 20:24:17 +0100

    test

--- ofa_patched/imagenet_codebase/networks/mobilenet_v3.py
+++ ofa_patched/imagenet_codebase/networks/mobilenet_v3.py
@@ -161,7 +161,10 @@ class EEMobileNetV3(MyNetwork):
             final_expand_width = [960]#[feature_dim[0] * 6]
             last_channel = [1280]#[feature_dim[0] * 8]
             self.exit_list.append(ExitBlock(n_classes,final_expand_width,feature_dim,last_channel,dropout_rate))
-            self.exit_block = self.exit_list[i] # magic inside ?
+            #self.exit_block = self.exit_list[i] # magic inside ? Understand why with this line is working :)
+            
+        if (self.n_exit!=0):
+            self.exit_list = nn.ModuleList(self.exit_list)
         
         '''
 
