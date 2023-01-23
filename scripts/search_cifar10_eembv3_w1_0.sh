1ecc37df52c2bf5e921c51262014be86009b6cb1
Matteo <matteogambella@gmail.com>
2023-01-23 11:27:36 +0100

    Revert "test"
    This reverts commit 3073776c5549d51d2fab9e5031b9ca5e4967102f.

--- scripts/search_cifar10_eembv3_w1_0.sh
+++ scripts/search_cifar10_eembv3_w1_0.sh
@@ -1,5 +1,5 @@
 python msunas.py --sec_obj macs \
-              --n_gpus 8 --gpu 0 --n_workers 4 --n_epochs 5 \
+              --n_gpus 8 --gpu 1 --n_workers 4 --n_epochs 5 \
               --dataset cifar10 --n_classes 10 \
               --data ../data/cifar10 \
               --predictor as --supernet_path ./ofa_nets/ofa_eembv3 --pretrained  \
