3b7ac2f2d265a44412bc23238225b439fdac5f80
Matteo <matteogambella@gmail.com>
2023-01-24 12:02:30 +0100

    test

--- patch_ofa.sh
+++ patch_ofa.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf "venv/lib/python3.8/site-packages/ofa"
-cp -r ofa_patched venv/lib/python3.8/site-packages/ofa
+rm -rf "../venv/lib/python3.8/site-packages/ofa"
+cp -r ofa_patched ../venv/lib/python3.8/site-packages/ofa
 
