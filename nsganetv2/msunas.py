6f1e28585ef57f7c2ff3f31b5d76ee524f06f0b8
Matteo <matteogambella@gmail.com>
2023-01-25 09:29:04 +0100

    test

--- nsganetv2/msunas.py
+++ nsganetv2/msunas.py
@@ -73,8 +73,13 @@ class MSuNAS:
   
     def search(self):
 
+        it_start = 1
         if self.resume:
             archive = self._resume_from_dir()
+            split = self.resume.rsplit("_",1)
+            it_start = int(split[1]) + 1
+            print("IT START")
+            print(it_start)
         else:
             # the following lines corresponding to Algo 1 line 1-7 in the paper
             archive = []  # initialize an empty archive to store all trained CNNs
@@ -95,8 +100,9 @@ class MSuNAS:
         # reference point (nadir point) for calculating hypervolume
         ref_pt = np.array([np.max([x[1] for x in archive]), np.max([x[2] for x in archive])])
 
+
         # main loop of the search
-        for it in range(2, self.iterations + 1):
+        for it in range(it_start, self.iterations + 1):
 
             # construct accuracy predictor surrogate model from archive
             # Algo 1 line 9 / Fig. 3(a) in the paper
