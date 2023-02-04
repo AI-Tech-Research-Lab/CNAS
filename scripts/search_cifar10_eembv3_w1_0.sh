01a2393a54dde134af249e37d7978f36b15bb161
Matteo <matteogambella@gmail.com>
2023-02-04 21:08:44 +0100

    test

--- scripts/search_cifar10_eembv3_w1_0.sh
+++ scripts/search_cifar10_eembv3_w1_0.sh
@@ -1,4 +1,4 @@
-python msunas.py --resume ../results/cifar10-mbv3-adaptive/iter_18 --sec_obj macs \
+python msunas.py --resume ../results/cifar10-mbv3-adaptive/iter_19 --sec_obj macs \
               --n_gpus 8 --gpu 1 --n_workers 4 --n_epochs 5 \
               --dataset cifar10 --n_classes 10 \
               --data ../data/cifar10 \
