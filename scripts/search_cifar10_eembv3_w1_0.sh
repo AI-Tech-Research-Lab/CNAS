e911867e48ac0d2cf5fe8121167cd9e85936b23b
Matteo <matteogambella@gmail.com>
2023-02-06 18:41:20 +0100

    test

--- scripts/search_cifar10_eembv3_w1_0.sh
+++ scripts/search_cifar10_eembv3_w1_0.sh
@@ -1,7 +1,7 @@
-python msunas.py --resume ../results/cifar10-mbv3-adaptive/iter_29 --sec_obj macs \
+python msunas.py --resume ../results/cifar10-mbv3-adaptive/iter_30 --sec_obj macs \
               --n_gpus 8 --gpu 1 --n_workers 4 --n_epochs 5 \
               --dataset cifar10 --n_classes 10 \
               --data ../data/cifar10 \
               --predictor as --supernet_path ./ofa_nets/ofa_eembv3 --pretrained  \
-              --save ../results/cifar10-mbv3-adaptive --iterations 1 --vld_size 5000 \
+              --save ../results/cifar10-mbv3-adaptive --iterations 0 --vld_size 5000 \
               --lr 40 --ur 104
