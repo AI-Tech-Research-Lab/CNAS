ed77a9940bff908a1d78554411687fc00119a69f
Matteo <matteogambella@gmail.com>
2023-02-09 19:33:38 +0100

    rename ofa_patched

--- nsganetv2/test.py
+++ nsganetv2/test.py
@@ -1,25 +1,27 @@
-from search_space.ofa import OFASearchSpace
+#from search_space.ofa import OFASearchSpace
 from evaluator import OFAEvaluator,get_net_info
-from ofa.elastic_nn.modules.dynamic_layers import ExitBlock
-import torch
-from ofa.model_zoo import ofa_net
-import numpy as np
-from utils import get_adapt_net_info,get_net_info
-from torchprofile import profile_macs
+#from ofa.elastic_nn.modules.dynamic_layers import ExitBlock
+#import torch
+#from ofa.model_zoo import ofa_net
+#import numpy as np
+from utils import get_net_info#,get_adapt_net_info
+#from torchprofile import profile_macs
+import json
 
-lr = 40
-ur = 100
-n_doe = 1
-#ss = OFASearchSpace('eemobilenetv3',lr,ur)
-
+#lr = 40
+#ur = 100
+#n_doe = 1
+subnet = './../results/cifar10-mbv3-adaptive/final/net-trade-off_1@17_NAS/net.subnet'
+supernet = './ofa_nets/ofa_mbv3_d234_e346_k357_w1.0'
+pretrained = True
+config = json.load(open(subnet))
+n_classes = 10
+evaluator = OFAEvaluator(n_classes=n_classes, model_path=supernet, pretrained = pretrained)
 ofa = OFAEvaluator(n_classes=1000,
 model_path='./ofa_nets/ofa_mbv3_d234_e346_k357_w1.0',
 pretrained = True)
-ks = [3, 3, 5, 7, 3, 7, 7, 7, 7, 5, 5, 7, 7, 3, 7, 7]
-e = [6, 3, 3, 6, 3, 6, 6, 6, 6, 3, 6, 3, 6, 6, 3, 3]
-d = [2, 2, 4, 4, 4]
-input_shape = (3,40,40)
-subnet, _ = ofa.sample({'ks': ks, 'e': e, 'd': d})
+input_shape = (3,config['r'],config['r'])
+subnet, _ = ofa.sample({'ks': config['ks'], 'e': config['e'], 'd': config['d']})
 info = get_net_info(subnet,input_shape)
 
 #print(ss.initialize(n_doe))
