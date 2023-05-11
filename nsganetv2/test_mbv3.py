5845dbb92bf4720e1c014b49982d1e24f173f655
Matteo <matteogambella@gmail.com>
2023-05-11 15:00:08 +0200

    test

--- nsganetv2/test_mbv3.py
+++ nsganetv2/test_mbv3.py
@@ -2,7 +2,7 @@ from evaluator import OFAEvaluator
 from utils import get_net_info, get_adapt_net_info
 import json
 
-subnet = './../results/cifar10-mbv3-adaptive/final/net-trade-off_4@3_NAS/net.subnet'
+subnet = './../benchmarks/EDANAS/cifar10-mbv3-adaptive/final/net-trade-off_4@3_NAS/net.subnet'
 supernet = './ofa_nets/ofa_mbv3_d234_e346_k357_w1.0'
 pretrained = False
 config = json.load(open(subnet))
