3f972920d6d1062eda1358f593e245a74fca7a14
Matteo <matteogambella@gmail.com>
2023-05-11 19:45:03 +0200

    test

--- nsganetv2/test_mbv3.py
+++ nsganetv2/test_mbv3.py
@@ -2,6 +2,7 @@ from evaluator import OFAEvaluator
 from utils import get_net_info, get_adapt_net_info
 import json
 
+'''
 subnet = './../benchmarks/EDANAS/cifar10-mbv3-adaptive/final/net-trade-off_4@3_NAS/net.subnet'
 supernet = './ofa_nets/ofa_mbv3_d234_e346_k357_w1.0'
 pretrained = False
@@ -13,7 +14,17 @@ pretrained = pretrained)
 input_shape = (3,config['r'],config['r'])
 subnet, _ = ofa.sample({'ks': config['ks'], 'e': config['e'], 'd': config['d']})
 info = get_net_info(subnet,input_shape)
+'''
 
+subnet = './../benchmarks/EDANAS/cifar10-mbv3-adaptive/final/net-trade-off_4@3_NAS/net.subnet'
+pretrained = False
+config = json.load(open(subnet))
+n_classes = 10
+ofa = OFAMobileNetV3(n_classes=10)
+input_shape = (3,config['r'],config['r'])
+ofa.set_active_subnet(ks=config['ks'], e=config['e'], d=config['d'])
+subnet = ofa.get_active_subnet(preserve_weight=True)
+info = get_net_info(subnet,input_shape)
 
 
 
