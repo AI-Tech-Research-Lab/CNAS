fe3299a0f8c3aa52f2981f44abccc74f36cdc0b1
Matteo <matteogambella@gmail.com>
2023-06-09 10:03:48 +0200

    update script

--- scripts/search_cifar10_eembv3_w1_0.sh
+++ scripts/search_cifar10_eembv3_w1_0.sh
@@ -1,7 +1,7 @@
-python msunas.py --resume ../results/edanas-r32/iter_1 --sec_obj macs \
+python msunas.py --resume ../results/edanas-r32/iter_18 --sec_obj macs \
               --n_gpus 1 --gpu 1 --n_workers 4 --n_epochs 5 \
               --dataset cifar10 --n_classes 10 \
               --data ../data/cifar10 \
               --predictor mlp --supernet_path ./ofa_nets/ofa_eembv3 --pretrained  \
-              --save ../results/edanas-r32 --iterations 30 --vld_size 5000 \
+              --save ../results/edanas-r32 --iterations 12 --vld_size 5000 \
               --lr 32 --ur 32
