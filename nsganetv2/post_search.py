5576dadcfc98fcc48c43322629479326c7552060
Matteo <matteogambella@gmail.com>
2023-10-07 11:49:55 +0200

    Update post_search.py

--- nsganetv2/post_search.py
+++ nsganetv2/post_search.py
@@ -79,6 +79,12 @@ def main(args):
         err_top1 = v[1]
         if(err_top1==100):
           archive.remove(v)
+
+    print("ARCHIVE")
+    print(v[0])
+    print(v[1])
+    print(v[2])
+    print(v[3])
     
     subnets, top1, sec_obj, util = [v[0] for v in archive], [v[1] for v in archive], [v[2] for v in archive], [v[3] for v in archive]
 
