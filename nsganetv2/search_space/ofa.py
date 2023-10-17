6ff161637b6f109706940cf042ac25b71518b819
Matteo <matteogambella@gmail.com>
2023-10-17 22:38:48 +0200

    Update ofa.py

--- nsganetv2/search_space/ofa.py
+++ nsganetv2/search_space/ofa.py
@@ -60,8 +60,9 @@ class OFASearchSpace:
 
             if (self.supernet == 'eemobilenetv3'):
                 while True:
+                    np.random.seed()
                     thresholds = np.random.choice(t, size=(len(depth)-1), replace=True).tolist()
-                    if any(t != 1.0 for t in thresholds):
+                    if any(t != 1 for t in thresholds):
                         break
                 print("THRESHOLDS: ", thresholds)
                 data.append({'ks': kernel_size, 'e': exp_ratio, 'd': depth, 't': thresholds, 'r': resolution})
