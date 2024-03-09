d6058d7cab224163b2a659d604c756936a8500fe
Matteo Gambella <55230920+matteogambella@users.noreply.github.com>
2024-03-09 19:14:57 +0100

    remove ofa folder

--- ofa/elastic_nn/networks/__init__.py
+++
@@ -1,9 +0,0 @@
-# Once for All: Train One Network and Specialize it for Efficient Deployment
-# Han Cai, Chuang Gan, Tianzhe Wang, Zhekai Zhang, Song Han
-# International Conference on Learning Representations (ICLR), 2020.
-
-from .ofa_proxyless import OFAProxylessNASNets
-from .ofa_mbv3 import OFAMobileNetV3, OFAEEMobileNetV3
-from .ofa_resnets import OFAResNets
-from .ofa_resnets_he import OFAResNetsHE
-from .ofa_mbv3_he import OFAMobileNetV3HE
