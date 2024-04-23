5844e7150ec38613b7138ecbda95ce8182d5979d
matteogambella <matteogambella@gmail.com>
2024-04-23 20:04:08 +0000

    update

--- duplicates.py
+++
@@ -1,9 +0,0 @@
-from cnas import CNAS
-
-#create a kwargs arg for cnas
-kwargs = {'first_obj': 'top1', 'sec_obj':'params', 'resume': '../results/nasbench/iter_12'}
-cnas = CNAS(kwargs)
-archive = cnas._resume_from_dir()
-print(archive[-10:])
-
-
