c8af92b48aa4689869aa8fbea1da535ea79bf979
fabriziopittorino <fabrizio.pittorino@gmail.com>
2024-10-03 12:17:22 +0200

    delete test files

--- cp_exps.py
+++
@@ -1,5 +0,0 @@
-from EarlyExits.utils_ee import cp_exps
-
-
-#cp_exps('results/search_nachos_nopeak_imagenette/iter_0', 'results/search_nachos_constraints_imagenette',only_backbone=False)
-cp_exps('results/search_nachos_noconstraints_imagenette/iter_0', 'results/search_nachos_nopeak_imagenette',only_backbone=False)
